6918aa5fcd46fc2a0b247d5dde656b3a50ea12ab
Akshat-pixel <65018836+Akshat-pixel@users.noreply.github.com>
2025-02-21 12:42:14 +0530

    Update ansiblesetup.sh

--- ansiblesetup.sh
+++ ansiblesetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update 
+sudo apt update 
 sudo apt install software-properties-common -y 
 sudo add-apt-repository --yes --update ppa:ansible/ansible -y 
 sudo apt install ansible -y 
